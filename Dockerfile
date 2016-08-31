cf8d76d5233e4bd5980975db9bec593264f14eb1
Nick Hynes <nhynes@mit.edu>
2016-08-31 20:48:51 +0000

    Basic working config

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,23 @@
+# docker run \
+#     -p 8080:8080 -p 8081:8081 -p 80:8000 \
+#     -e "HUB_IP=$(ip -o -4 a show eth0 | awk '{print $4}' | sed 's/\/[0-9]\+$//')" \
+#     -e "OPEN_SESAME=$SECRET"
+#     nhynes/jupyterhub
+
 FROM jupyterhub/jupyterhub
 
 MAINTAINER Nick Hynes <nhynes@mit.edu>
 
 RUN pip install --upgrade pip
-RUN pip install \
-    docker-py==1.9.0 \
-    git+git://github.com/jupyter/dockerspawner.git \
-    jupyter_client
+RUN pip install docker-py==1.9.0
+RUN pip install git+git://github.com/jupyter/dockerspawner.git
 
 # expose hub service, proxy, and api, respectively
 EXPOSE 8080
 EXPOSE 8000
 EXPOSE 8081
 
-COPY jupyterhub_config.py /srv/jupyterhub/jupyterhub_config.py
 COPY sesameauthenticator.py /srv/jupyterhub/sesameauthenticator.py
+COPY jupyterhub_config.py /srv/jupyterhub/jupyterhub_config.py
 
 ENTRYPOINT ["jupyterhub", "-f", "/srv/jupyterhub/jupyterhub_config.py"]
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -11,9 +11,9 @@ c.JupyterHub.log_level = "INFO"
 c.JupyterHub.confirm_no_ssl = True
 
 # need to listen on all ifaces because it's running in a container
-# requires setting HUB_IP and DOCKER_HOST via env vars
+# requires setting HUB_IP via env vars
 c.JupyterHub.proxy_api_ip = '0.0.0.0'
-c.JupyterHub.hub_ip = public_ips()[0]
+c.JupyterHub.hub_ip = '0.0.0.0'
 
 # Authenticator
 c.JupyterHub.authenticator_class = 'sesameauthenticator.SesameAuthenticator'
@@ -23,14 +23,14 @@ c.Authenticator.whitelist = set()
 c.Authenticator.admin_users = {'nhynes'}
 
 # Spawner
-c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
-c.DockerSpawner.container_image = 'dhunter/itorch-notebook'
+hub_ip = os.environ['HUB_IP']
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.container_image = 'jupyterhub/singleuser'
 c.DockerSpawner.container_ip = '0.0.0.0'
 c.DockerSpawner.remove_containers = True
-c.DockerSpawner.hub_ip_connect = c.JupyterHub.hub_ip
-os.environ['DOCKER_HOST'] = c.JupyterHub.hub_ip # one server to rule them all
-#c.DockerSpawner.volumes = {'/var/run/restuser':'/var/run/restuser.sock'}
+c.DockerSpawner.hub_ip_connect = hub_ip
+os.environ['DOCKER_HOST'] = hub_ip + ':3376' # one server to rule them all
 
 # Jupyterhub params that can also be set in /etc/ipython but maybe not because itorch
-c.Spawner.notebook_dir = '~/notebooks'
-c.Spawner.args = ['--NotebookApp.default_url=/notebooks/Welcome.ipynb']
+#c.Spawner.notebook_dir = '~/notebooks'
+#c.Spawner.args = ['--NotebookApp.default_url=/notebooks/Welcome.ipynb']
